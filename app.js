5f30448ad0978ac75c167167f1799f34f9aa5ae4
Torque <siddhesh1795@gmail.com>
2023-04-23 16:09:13 +0530

    Added Bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -25,65 +26,77 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
 
 app.get("/", function (req, res) {
-    
-   res.render("home");
+
+    res.render("home");
 });
 
 
 app.get("/login", function (req, res) {
-    
-   res.render("login");
+
+    res.render("login");
 });
 
 
 app.get("/register", function (req, res) {
-    
-   res.render("register");
+
+    res.render("register");
 });
 
 
 
 app.post("/register", function (req, res) {
-   
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save().finally(() => {
+            console.log("Succesfully registered new user.");
+        }).catch((err) => {
+            console.log(err);
+        }).then(() => {
+            res.render("secrets");
+        });
+
     });
-    newUser.save().finally(() => {
-        console.log("Succesfully registered new user.");
-    }).catch((err) => {
-        console.log(err);
-    }).then(() => {
-        res.render("secrets");
-    })
+
+
+
+
 });
 
 
 
 app.post("/login", function (req, res) {
-     
+
     const username = req.body.username;
     const password = req.body.password;
-    
+
     User.findOne({
         email: username
     }).finally(() => {
         console.log("Successfully fetched User details.");
     }).then((foundUser) => {
-     
-       if ( foundUser.password === password ) {
-            
-           res.render("secrets");
-           
-       } else {
-           res.send("Wrong Password");
-        };
+
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+            // result == true
+            if (result === true) {
+                res.render("secrets");
+            } else {
+                res.redirect("/login");
+            };
+        });
+
+
+
 
 
     }).catch((err) => {
