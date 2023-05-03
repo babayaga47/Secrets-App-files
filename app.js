258e56b46530eb7f8080bb1493da9e1992caa9a4
Torque <siddhesh1795@gmail.com>
2023-05-03 23:17:01 +0530

    Added Facebook Login method

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const FacebookStrategy = require("passport-facebook");
 
 
 
@@ -34,7 +35,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -74,6 +76,19 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get("/", function (req, res) {
 
     res.render("home");
@@ -90,7 +105,17 @@ app.get("/auth/google/secrets",
     // Successful authentication, redirect home.
       res.redirect("/secrets");
   });
+  
+  
+  app.get("/auth/facebook",
+  passport.authenticate("facebook"));
 
+  app.get("/auth/facebook/secrets",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 
 
